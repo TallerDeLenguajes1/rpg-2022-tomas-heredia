bf0927c2a324144759e6d103435751d115ed1e29
tomas-heredia <tomasheredia04@hotmail.com>
2022-05-31 16:50:35 -0300

    random de estadisticas

---
+++ Myapp/Personaje.cs
@@ -0,0 +1,17 @@
+namespace RPG;
+
+
+public class Personaje{
+    public Personaje(){}
+    public int Velocidad;
+    public int Destresa;
+    public int Fuerza;
+    public int Nivel;
+    public int Armadura;
+    public string[] tipo = {"Guerrero","Ladron","Paladin"};
+    public string Nombre;
+    public string Apodo;
+    public DateTime Nacimiento;
+    public int Edad;
+    public int Salud;
+}
--- Myapp/Program.cs
+++ Myapp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using RPG;
+public class program
+{
+    public static string[] Nombres = {"Tomas", "Irina","Gonsalo","Maria"};
+     public static string[] Apodos = {"Aniquilador", "Fachero","TuNoMeteCabraSarabanbichuie","Pablo"};
+    public static void Main(String[] args){
+        //creo personajes
+        Personaje personaje1 = new Personaje(); 
+        Personaje personaje2 = new Personaje();
+    }
+
+    public static Personaje CreacionAleatoria(){
+        
+        Random rand = new Random();
+        Personaje personajeAux = new Personaje();
+        personajeAux.Salud = 100;
+        personajeAux.Armadura = rand.Next(1,10);
+        personajeAux.Destresa = rand.Next(1,10);
+        personajeAux.Fuerza = rand.Next(1,10);
+        personajeAux.Nivel = rand.Next(1,10);
+        personajeAux.Velocidad = rand.Next(1,10);
+        personajeAux.Nombre = Nombres[rand.Next(0,Nombres.Length)];
+        personajeAux.Apodo = Apodos[rand.Next(0,Nombres.Length)];
+
+
+
+        return null;
+    }
+}
+
